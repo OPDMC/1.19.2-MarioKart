dd12d8ba0f3cf86d2fc3fe702262f5627a5e7d80
SynthesisDu <75297777+TitanRGB@users.noreply.github.com>
2023-04-09 07:16:26 -0400

    Add bat

---
+++ bin/docker_windev_buildtest.bat
@@ -0,0 +1,1 @@
+docker build -t 1.19.2-mariokart-test:test -f ../Dockerfile .
---
+++ bin/docker_windev_dmcs.bat
@@ -0,0 +1,4 @@
+set port=3000
+set mc_version=1.19.2
+set java_version=java17-jdk
+docker run -it --rm -v %cd%\..\:/data -w /data -p %port%:25565 -e VERSION=%mc_version% -e EULA=TRUE itzg/minecraft-server:%java_version%
