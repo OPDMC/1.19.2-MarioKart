0004983dce3c90161704632b7c564cc8c64eef6d
SynthesisDu <75297777+TitanRGB@users.noreply.github.com>
2023-04-10 22:09:55 -0400

    start.sh

--- bin/init.sh
+++ bin/init.sh
@@ -1,8 +1,8 @@
 if [ ! -f /minecraft/server.properties ]; then
   mkdir /minecraft
   mv /docker_init/* /minecraft/
-  java -Xmx2G -Xms2G -jar /minecraft/minecraft_server.1.19.2.jar nogui
+  sh /minecraft/start.sh
 else
   rm -rf /docker_init/*
-  java -Xmx2G -Xms2G -jar /minecraft/minecraft_server.1.19.2.jar nogui
+  sh /minecraft/start.sh
 fi
---
+++ minecraft_server/start.sh
@@ -0,0 +1,2 @@
+#!/usr/bin/env bash
+java -Xmx2G -Xms2G -jar /minecraft/minecraft_server.1.19.2.jar nogui
